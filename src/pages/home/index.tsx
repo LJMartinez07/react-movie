391d9e2cd60bd0abb08af5467fa4963cee5f13f0
mundomovilmh <luis.jorge.martinez28@gmail.com>
2020-08-12 08:37:43 -0400

    center text description in home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,15 +7,17 @@ export default () => {
                 textAlign: 'center',
                 display: 'flex',
                 flexDirection: 'column',
-                justifyContent: 'space-around',
+                justifyContent: 'center',
                 height: '100vh',
             }}
         >
-                <div >
+            <div>
+                <div style={{maxWidth:600+'px', margin: '0px auto' }}>
                     <h1>React Movies</h1>
                     <h4 style={{color: '#5c255c'}}>
                         The Movie Database (TMDb) is a community built movie and TV database. Every piece of data has been added by our amazing community dating back to 2008. TMDb's strong international focus and breadth of data is largely unmatched and something we're incredibly proud of. Put simply, we live and breathe community and that's precisely what makes us different.
                     </h4>
+                </div>
                 </div>
             <div>
                 <a href={'https://github.com/LuisMartinez28/react-movie'}>
