d40a46084481970770f88be79e0c8936104f4c22
mundomovilmh <luis.jorge.martinez28@gmail.com>
2020-08-17 11:05:30 -0400

    creating routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/home/';
+import Movies from './pages/movies'
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Luis Martinez</p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/Home" render={() => <Home/>} />
+                <Route path="/movies" render={() => <Movies/>} />
+            </Switch>
+        </Router>
     );
 }
 
