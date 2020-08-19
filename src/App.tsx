c72792df66022aad5cb2410fc5c14b15e31f21f1
mundomovilmh <luis.jorge.martinez28@gmail.com>
2020-08-19 14:01:53 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import RouterView from './router/router-view';
 function App() {
     return (
         <React.StrictMode>
-            
+            <div className="app">
+                <RouterView />
+            </div>
         </React.StrictMode>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import RouterView from './router/router-view';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-ReactDOM.render((
-    <div className="app">
-                <RouterView />
-            </div>
-), document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
