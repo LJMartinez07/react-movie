fb25237fb9a757e8e5c67c8a492c0d7769738616
Luis Martinez <mrluismartinez28@gmail.com>
2020-08-24 22:09:09 -0400

    adding title movie

--- src/pages/movies/index.tsx
+++ src/pages/movies/index.tsx
@@ -13,7 +13,7 @@ export default () => {
 
     return (
         <Main>
-            {/* <h1>Movies</h1> */}
+            <h1>Popular movies</h1>
             <MovieListComponent movieList={movies} />
         </Main>
     );
