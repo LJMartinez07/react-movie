7d00b9e9997fe5b0d05d7c37efe8b5708c7162cc
Luis Martinez <mrluismartinez28@gmail.com>
2020-08-28 21:26:44 -0400

    changin latest to top rated

--- src/pages/movies/index.tsx
+++ src/pages/movies/index.tsx
@@ -4,14 +4,14 @@ import { MovieListComponent } from '../../components/movie/movie-list'
 export default () => {
     const MOVIE_LIST_TYPE = {
         POPULAR: 'popular',
-        LATEST: 'top_rated'
+        TOP_RATED: 'top_rated'
     }
     return (
         <Main>
             <h1>Popular Movies</h1>
             <MovieListComponent type={MOVIE_LIST_TYPE.POPULAR} />
             <h1>Top Rated</h1>
-            <MovieListComponent type={MOVIE_LIST_TYPE.LATEST} />
+            <MovieListComponent type={MOVIE_LIST_TYPE.TOP_RATED} />
         </Main>
     );
 };
