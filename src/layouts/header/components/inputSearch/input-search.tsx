86aab89c199845d7c9656324094f712faf7aa90b
mundomovilmh <luis.jorge.martinez28@gmail.com>
2020-09-03 17:43:17 -0400

    open and show suggestions

--- src/layouts/header/components/inputSearch/input-search.tsx
+++ src/layouts/header/components/inputSearch/input-search.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import styles from './input-search.module.scss';
 import MovieService from '../../../../http/service/MovieService';
 import { KeyWordsComponent } from '../keyWords/key-word';
@@ -16,29 +17,36 @@ interface res {
 export const InputSearchComponent: FC = () => {
     const service = new MovieService();
     const [query, setQuery] = useState('');
+    const [open, setOpen] = useState(false);
     const [suggestions, setSuggestions] = useState([]);
-    // useEffect(() => {
 
-    //     // setSuggestions(service.getKeywords(query));
-    // }, [query]);
     function handleClick(event: any) {
         setQuery(event.target.value);
         if (event.target.value)
             service.getKeywords(event.target.value).then((res) => {
-                // console.log(res);
                 setSuggestions(res.results);
             });
+    }
+
+    function openSuggestion() {
+        setOpen(true);
+    }
+
+    function hideSuggestion() {
+        setOpen(false);
     }
     return (
         <div className={styles.input_search}>
             <input
+                onBlur={hideSuggestion}
+                onFocus={openSuggestion}
                 className="bp3-input"
                 value={query}
                 onChange={handleClick}
                 placeholder="Search your favorite Movie/Tv Show"
                 type="search"
             />
-            <KeyWordsComponent suggestions={suggestions} />
+            {open == true && <KeyWordsComponent suggestions={suggestions} />}
         </div>
     );
 };
