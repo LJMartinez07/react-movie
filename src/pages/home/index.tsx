706968e2fc317f334c2a381eeabc2887aa001353
mundomovilmh <luis.jorge.martinez28@gmail.com>
2020-09-10 08:19:52 -0400

    fixing error

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -17,7 +17,7 @@ export default () => {
                     <div style={{ maxWidth: 600 + 'px', margin: '0px auto' }}>
                         <h1>React Movies</h1>
                         <h4 style={{ color: '#5c255c' }}>
-                            The Movie Databasse (TMDb) is a community built movie and TV database. Every piece of data
+                            The Movie Database (TMDb) is a community built movie and TV database. Every piece of data
                             has been added by our amazing community dating back to 2008. TMDb's strong international
                             focus and breadth of data is largely unmatched and something we're incredibly proud of. Put
                             simply, we live and breathe community and that's precisely what makes us different.
