f5296b927f7aed3a5cf5ea5a92bf985a92f378c2
mundomovilmh <luis.jorge.martinez28@gmail.com>
2020-09-15 15:56:22 -0400

    removing console logs

--- src/router/router-view.tsx
+++ src/router/router-view.tsx
@@ -3,27 +3,20 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import routerMap from './config';
 function CustomRoute(props: iRouterViewProps) {
     let path: string = props.location.pathname;
-    // props.beforeEnter && props.beforeEnter(path);
-    console.log(path);
+    props.beforeEnter && props.beforeEnter(path);
     // '/'-> '/home
     if (path === '/') return <Redirect to="/react-movie/home" />;
     // if can match
     let matchRoute: any = routerMap.find((item) => {
         let url = item.path;
-
         // /movies/:id -> \\/movies\\/[^/+]
         url = url.replace(/(:.+)/g, '[^/]+').replace(/\//g, '\\/');
-        // console.log(new RegExp(`${url}(\\/|\\/)?$`, 'gi').test(path));
-        // console.log(url + '---' + path);
         return new RegExp(`${url}(\\/|\\/)?$`, 'gi').test(path);
     });
-
-    // console.log(path + '---' + matchRoute);
     if (matchRoute) {
-        console.log(matchRoute.path);
         return <Route exact={!matchRoute.hasChild} path={matchRoute.path} component={matchRoute.component} />;
     }
-    return <Redirect to="/react-movie/404" />;
+    return <Redirect to="/404" />;
 }
 
 export const RouterView: React.FC<iRouterViewProps> = (props) => {
